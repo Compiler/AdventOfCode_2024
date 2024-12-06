5bf6a115ba3873e92b83a1361c4a240767613142
Compiler <LukeRocheDev@gmail.com>
2024-12-06 09:07:20 -0600

    Part1 greedy

--- Luke/Day5/day5_1.cpp
+++ Luke/Day5/day5_1.cpp
@@ -1,5 +1,7 @@
 
 
+#include "../helper.h"
+#include <algorithm>
 #include <fstream>
 #include <sstream>
 #include <string>
@@ -20,18 +22,33 @@ int main() {
             int pivot = line.find('|');
             std::string key = line.substr(0, pivot); 
             std::string val = line.substr(pivot + 1, line.size()); 
-            printf("Rule 1: %s comes before %s\n", key.c_str(), val.c_str());
+            printf("Rule %ld: %s comes before %s\n", rules.size() + 1, key.c_str(), val.c_str());
             rules[key] = val;
         } else {// Pages
+            if(line.empty()) continue;
+            printf("Page: '%s'\n", line.c_str());
+            int numEntries = std::count(line.begin(), line.end(), ',') + 1;
+            int middleIndex = numEntries / 2;
+            int middleEntry = 0;
             std::stringstream pageStream(line);
             std::unordered_set<std::string> seenBefore;
             std::string entry;
             bool validEntry = true;
+            int index = 0;
+            // Place an entry. While doing so, if we have ever seen it's rule-> invalid page.
             while (std::getline(pageStream, entry, ',')) {
-                auto seen = seenBefore.find(entry);
-
+                if(index == middleIndex) middleEntry = std::stoi(entry);
+                auto ruleIt = rules.find(entry);
+                const auto ruleStr = ruleIt != rules.end() ? ruleIt->second : ""; 
+                const bool ruleSeenBefore = ruleIt != rules.end() && seenBefore.find(ruleStr) != seenBefore.end();
+                const auto seenEntryBefore = seenBefore.find(entry) != seenBefore.end();
+                if(ruleSeenBefore) validEntry = false;
+                seenBefore.insert(entry);
+                index++;
             }
-            numValid += validEntry;
+            printf("Middle: %d\n", middleEntry);
+            numValid += validEntry * middleEntry;
         }
     }
+    printf("Sum of valid middle entries: %d\n", numValid);
 }
