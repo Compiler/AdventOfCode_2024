fcf9b9fdd52e1afc81fdb21bac66dde64844ad35
Compiler <LukeRocheDev@gmail.com>
2024-12-06 09:09:41 -0600

    Angry. Stuck on this forever before I realized multiple rules for one key...

--- Luke/Day5/day5_1.cpp
+++ Luke/Day5/day5_1.cpp
@@ -7,13 +7,14 @@
 #include <string>
 #include <unordered_map>
 #include <unordered_set>
+#include <vector>
 int main() {
     
     std::ifstream file("puzzle_input.txt");
 
     std::string line;
     bool inStart = true;
-    std::unordered_map<std::string, std::string> rules;
+    std::unordered_map<std::string, std::vector<std::string>> rules;
     int numValid = 0;
     while(std::getline(file, line)) {
         inStart = inStart && line != "";
@@ -22,11 +23,9 @@ int main() {
             int pivot = line.find('|');
             std::string key = line.substr(0, pivot); 
             std::string val = line.substr(pivot + 1, line.size()); 
-            printf("Rule %ld: %s comes before %s\n", rules.size() + 1, key.c_str(), val.c_str());
-            rules[key] = val;
+            rules[key].push_back(val);
         } else {// Pages
             if(line.empty()) continue;
-            printf("Page: '%s'\n", line.c_str());
             int numEntries = std::count(line.begin(), line.end(), ',') + 1;
             int middleIndex = numEntries / 2;
             int middleEntry = 0;
@@ -39,14 +38,15 @@ int main() {
             while (std::getline(pageStream, entry, ',')) {
                 if(index == middleIndex) middleEntry = std::stoi(entry);
                 auto ruleIt = rules.find(entry);
-                const auto ruleStr = ruleIt != rules.end() ? ruleIt->second : ""; 
-                const bool ruleSeenBefore = ruleIt != rules.end() && seenBefore.find(ruleStr) != seenBefore.end();
-                const auto seenEntryBefore = seenBefore.find(entry) != seenBefore.end();
-                if(ruleSeenBefore) validEntry = false;
+                for(auto rule : ruleIt->second){
+                    const auto ruleStr = rule; 
+                    const bool ruleSeenBefore = ruleIt != rules.end() && seenBefore.find(ruleStr) != seenBefore.end();
+                    const auto seenEntryBefore = seenBefore.find(entry) != seenBefore.end();
+                    if(ruleSeenBefore) validEntry = false;
+                }
                 seenBefore.insert(entry);
                 index++;
             }
-            printf("Middle: %d\n", middleEntry);
             numValid += validEntry * middleEntry;
         }
     }
