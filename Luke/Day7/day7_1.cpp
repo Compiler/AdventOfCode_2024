805b049be5c20e5d8027bc653b654960a195643c
Compiler <LukeRocheDev@gmail.com>
2024-12-16 08:04:07 -0600

    Cleanup

--- Luke/Day7/day7_1.cpp
+++ Luke/Day7/day7_1.cpp
@@ -5,7 +5,7 @@
 typedef unsigned long long ull;
 
 bool evaluate(std::vector<ull>& nums, ull target, ull total = 0, ull index = 0) {
-    if(target == total)      return true;
+    if(index == nums.size()) return target == total;
     if(index >= nums.size()) return false;
 
     if(index == 0) {
@@ -21,21 +21,16 @@ int main() {
     std::ifstream file("puzzle_input.txt");
 
     std::string line;
-
     ull sumTestValues = 0;
-
     while(std::getline(file, line)) {
         ull testValue = std::stoll(line.substr(0, line.find(':')));
+
         std::istringstream stream(line.substr(line.find(':') + 1));
         std::vector<ull> numbers;
         ull number;
-        while(stream >> number) {
-            numbers.push_back(number);
-        }
-
-        if (evaluate(numbers, testValue)) {
-            sumTestValues += testValue;
-        }
+        
+        while(stream >> number) numbers.push_back(number);
+        if (evaluate(numbers, testValue)) sumTestValues += testValue;
     }
 
     printf("Sums of testvalues: %llu\n", sumTestValues);
