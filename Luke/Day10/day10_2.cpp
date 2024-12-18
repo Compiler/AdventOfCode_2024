9e7f4f182f5a35154775bb925608a18e6accb9f9
Compiler <LukeRocheDev@gmail.com>
2024-12-18 14:51:21 -0600

    Part 2, going to laptop again

---
+++ Luke/Day10/day10_2.cpp
@@ -0,0 +1,96 @@
+#include <climits>
+#include <fstream>
+#include <functional>
+#include <queue>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+typedef std::vector<std::vector<int>> Graph;
+struct Point{ 
+    int x,y;
+
+    bool operator==(const Point& other) const {
+        return other.x == x && other.y == y;
+    }
+
+    Point operator+(const Point& other) {
+        return {other.x + x, other.y + y};
+    }
+
+    std::size_t operator()() const {
+        std::size_t h1 = std::hash<int>{}(x); 
+        std::size_t h2 = std::hash<int>{}(y);  
+
+        return h1 ^ (h2 << 1);
+    }
+    std::size_t operator()(const Point& p) const {
+        std::size_t h1 = std::hash<int>{}(p.x); 
+        std::size_t h2 = std::hash<int>{}(p.y);  
+
+        return h1 ^ (h2 << 1);
+    }
+
+};
+
+std::vector<Point> directions = {{1,0}, {-1,0}, {0, 1}, {0, -1}};
+
+bool inBounds(const Graph& g, const Point& p) {
+    return p.y >= 0 && p.y < g.size() && g[p.y].size() > p.x && p.x >= 0;
+}
+
+int getCell(const Graph& g, const Point& p) {
+    if(!inBounds(g, p)) return INT_MIN;
+    return g[p.y][p.x];
+}
+
+std::unordered_set<std::string> paths;
+int run_trail(Graph& graph, Point position, std::string path = "") {
+    if(!inBounds(graph, position)) return 0;
+    int currentHeight = getCell(graph, position);
+    if(currentHeight == 9){
+        if(paths.find(path) != paths.end()) return 0;
+        paths.insert(path);
+        return 1;
+    }
+    path += "(" + std::to_string(position.x) + "," + std::to_string(position.y) + ")";
+    printf("(%d, %d) = %d\n", position.x, position.y, currentHeight);
+
+    // Run in all directions, backtrack at top.
+    int total = 0;
+    for(auto& newDirection : directions) {
+        auto nextPos = position + newDirection;
+        auto nextCell = getCell(graph, nextPos);
+        if(nextCell == currentHeight + 1) {
+            total += run_trail(graph, nextPos, path);
+        }
+    }
+
+    return total;
+}
+
+int main() {
+    std::ifstream file("puzzle_input.txt");
+    std::string line;
+     
+    std::vector<Point> trailHeads;
+    // Build graph
+    Graph graph;
+    while (std::getline(file, line)) {
+        graph.push_back({});
+        for(auto c : line) {
+            if(c == '0') trailHeads.push_back({(int)graph.back().size(), (int)graph.size() - 1});
+            graph.back().push_back(c - '0');
+        }
+    }
+
+    int trailScore = 0;
+    for(auto& trailHead : trailHeads) {
+        printf("Running trail (%d, %d)\n", trailHead.x, trailHead.y);
+        trailScore += run_trail(graph, trailHead);
+    }
+    printf("Trail scores sum: %d\n", trailScore);
+
+
+}
